84d1c6b31c55da7ca5ad7909d3e5377353185ce4
Alex Ayza <alexayzaleon@gmail.com>
2020-12-19 13:16:50 +0100

    Reduced error by deleting edge cases. Error: 354€

--- lineal_regression/test.py
+++ lineal_regression/test.py
@@ -12,15 +12,20 @@ print(theta)
 data = np.genfromtxt('data/barcelona_apartments.csv', delimiter=',', skip_header=True)
 errors = 0
 
+max_val = 5
+apts = []
+
 for row in data:
     test = row.copy()
+    if test[0]/test[3] < max_val:
+        apts.append(test.copy())
     test[0] = 1
     test[1] = (test[1] - mean[1]) / standard_deviation[1]
     test[2] = (test[2] - mean[2]) / standard_deviation[2]
     test[3] = (test[3] - mean[3]) / standard_deviation[3]
     price = np.dot(test, theta)
-    print(str(price) + " - " + str(row[0]) + " ERROR: " + str(abs(price - row[0])))
     errors += abs(price - row[0])
 
 errors /= data.shape[0]
 print(errors)
+print(apts)
